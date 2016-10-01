a373c73ff5c1242d6fe2eb820312e79b2bca27e6
Thibault PLET <thibault.plet@gmail.com>
2016-10-01 17:35:12 +0200

    Add ArduinoActionManager to add generic arduino features

---
+++ src/com/osteres/automation/arduino/action/ArduinoActionManager.h
@@ -0,0 +1,92 @@
+//
+// Created by Thibault PLET on 02/06/2016.
+//
+
+#ifndef COM_OSTERES_AUTOMATION_ARDUINO_ACTION_ARDUINOACTIONMANAGER_H
+#define COM_OSTERES_AUTOMATION_ARDUINO_ACTION_ARDUINOACTIONMANAGER_H
+
+#include <Arduino.h>
+#include <string>
+#include <com/osteres/automation/transmission/packet/Command.h>
+#include <com/osteres/automation/transmission/packet/CommandString.h>
+#include <com/osteres/automation/transmission/packet/Packet.h>
+#include <com/osteres/automation/action/ActionManagerBase.h>
+#include <com/osteres/automation/arduino/action/SensorIdentifierAction.h>
+
+using com::osteres::automation::transmission::packet::Command;
+using com::osteres::automation::transmission::packet::CommandString;
+using com::osteres::automation::transmission::packet::Packet;
+using com::osteres::automation::action::ActionManagerBase;
+using com::osteres::automation::arduino::action::SensorIdentifierAction;
+using std::string;
+
+namespace com
+{
+    namespace osteres
+    {
+        namespace automation
+        {
+            namespace arduino
+            {
+                namespace action
+                {
+                    class ArduinoActionManager : public ActionManagerBase
+                    {
+                    public:
+                        /**
+                         * Constructor
+                         */
+                        ArduinoActionManager() {
+                        }
+
+                        /**
+                         * Process packet
+                         */
+                        virtual void processPacket(Packet * packet)
+                        {
+                            // Command: IDENTIFIER_RESPONSE
+                            if (Command::IDENTIFIER_RESPONSE == packet->getCommand()) {
+                                if (this->hasActionSensorIdentifier()) {
+                                    this->getActionSensorIdentifier()->response(packet);
+                                }
+                            }
+                        }
+
+                        /**
+                         * Set sensor identifier action
+                         */
+                        void setActionSensorIdentifier(SensorIdentifierAction * action)
+                        {
+                            this->sensorIdentifierAction = action;
+                        }
+
+                        /**
+                         * Flag to indicate if sensor identifier action is defined
+                         */
+                        bool hasActionSensorIdentifier()
+                        {
+                            return this->sensorIdentifierAction != NULL;
+                        }
+
+                        /**
+                         * Get sensor identifier action
+                         */
+                        SensorIdentifierAction * getActionSensorIdentifier()
+                        {
+                            return this->sensorIdentifierAction;
+                        }
+
+                    protected:
+
+                        /**
+                         * Sensor identifier action
+                         */
+                        SensorIdentifierAction * sensorIdentifierAction = NULL;
+                    };
+                }
+            }
+        }
+    }
+}
+
+#endif //COM_OSTERES_AUTOMATION_ARDUINO_ACTION_ARDUINOACTIONMANAGER_H
